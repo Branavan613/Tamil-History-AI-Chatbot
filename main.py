69a9034a658e776e56860cf8527016ffb216c7db
Branavan613 <108775799+Branavan613@users.noreply.github.com>
2024-08-15 20:28:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
-from groq import Groq
+import asyncio
 import os
+from groq import AsyncGroq
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 import ollama
 import chromadb
-import time
+from dotenv import load_dotenv
 
-
-# Initialize the PersistentClient
+# Calls the PersistentClient Vector Store
 client = chromadb.PersistentClient(path=".")
 collection_name = "docs"
 collection = client.get_collection(name=collection_name)
+load_dotenv()
 
-# print(collection.count())
+async def multiquery(client, model, user_question):
+    """
+    This function takes the initial user question and reinterprets it into 3 more specific questions. In the case that a user's question is too 
+    broad or bland, these new question will create a more accurate search radius and will pull more relevant chunks of data
+    client (Groq): The Groq client used to interact with the untrained model.
+    model (str): The name of the untrained model.
+    user_question (str): The question asked by the user.
+    Returns:
+    list: A list of 3 relevant questions.
+    """
+    query_prompt = f'''
+    Your task is to generate 3 different search queries that aim to answer the user question from multiple perspectives. Each query MUST tackle the question from a different viewpoint, we want to get a variety of RELEVANT search results. Provide these alternative questions seperated by newlines.
+    Your answer should be depicted in the format shown only between the angled brackets:
+    <
+    query 1
+    query 2
+    query 3
+    >
+    Original question: {user_question}
+    '''
+    chat_completion = await client.chat.completions.create(
+        messages= [
+            {"role": "system", "content" : query_prompt}
+        ],
+        model=model,
+        temperature = 0,
+        stream = False
+    )
+    response = chat_completion.choices[0].message.content
+    # print(response)
+    response = response.split("\n")
+    return response[1:3]
 
 
-def ota_speech_chat_completion(client, model, user_question, relevant_excerpts):
+async def ota_speech_chat_completion(client, model, conversation_history, relevant_excerpts):
     """
     This function generates a response to the user's question using a pre-trained model.
     Parameters:
     client (Groq): The Groq client used to interact with the pre-trained model.
     model (str): The name of the pre-trained model.
-    user_question (str): The question asked by the user.
-    relevant_excerpts (str): A string containing the most relevant excerpts from articles.
+    conversation_history (dict): A dictionary that holds all the previous questions from the user and the previous responses from the model
     Returns:
-    str: A string containing the response to the user's question.
+    dict: A dictionary containing the response to the user's question in .choices[0].delta.content.
     """
 
-    # Define the system prompt
     system_prompt = '''
     You are teaching Sri Lankan history. Given the user's question and relevant excerpts from
     articles regarding Tamil and Sri Lankan history, provide concise answers in a way understandable
@@ -36,70 +66,101 @@ def ota_speech_chat_completion(client, model, user_question, relevant_excerpts):
     present in the excerpt and provide precise answers. However, do not explicitly mention the excerpt.
     Make sure your tone is serious and informational: no exclamation marks.
     '''
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "assistant", "content": relevant_excerpts}
+        ]
+    
+    for role, content in conversation_history:
+        messages.append({"role": role, "content": content})
+    chat_completion = await client.chat.completions.create(
+        messages= messages,
+        model=model,
+        temperature = 0, #randomness of model's answers set to 0
+        stream = True 
 
-    # Generate a response to the user's question using the pre-trained model
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": "User Question: " + user_question},
-            {"role": "assistant", "content": relevant_excerpts}
-        ],
-        model=model
     )
+    return chat_completion
 
-    # Extract the response from the chat completion
-    response = chat_completion.choices[0].message.content
-    print(f"time: {chat_completion.usage.total_time}")
-    print(f"tokens: {chat_completion.usage.total_tokens}")
-    return response
-
-def main():
+async def main():
     """
-    This is the main function that runs the application. It initializes the Groq client and the embedding model,
-    gets user input from the Streamlit interface, retrieves relevant excerpts from articles based on the user's question,
+    This is the main function that runs the application. It initializes the Groq client, 
+    retrieves relevant excerpts from articles based on the user's question,
     generates a response to the user's question using a pre-trained model, and displays the response.
     """
     model = 'llama3-8b-8192'
 
-    # Initialize the embedding function
-    # embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-    # Initialize the Groq client
-    groq_api_key = os.environ.get('GROQ_API_KEY')
-    groq_client = Groq(api_key=groq_api_key)
+    groq_api_key = os.getenv('GROQ_API_KEY')
+    groq_client = AsyncGroq(api_key=groq_api_key)
     
-    # print("OTA RAG")
     multiline_text = """
     Welcome! Ask me any question about Sri Lankan history and using information from hundreds of news reports, articles, and papers, will answer whatever question you have. I was built to provide specific information from a unbias point of view on historical events.
     """
     print(multiline_text)
+    
+    conversation_history = []
 
     while True:
         # Get the user's question
-        user_question = input("Ask a question: ")
+        user_question = input("\nAsk a question: ")
 
-        # Generate embedding for the user's question
-        response = ollama.embeddings(
+        conversation_history.append(("user", user_question))
+        # Generate multiquery for user question
+        multiqueries =  await multiquery(groq_client, model, user_question)
+        # print(multiqueries)
+
+        data = []
+        #there are instances where the untrained model will not deal with sensitive topics. In that case, just pass the original question into the trained RAG model
+        if multiqueries:
+            for query in multiqueries:
+                #create embedding for each prompt
+                response = ollama.embeddings(
+                    prompt=query,
+                    model="nomic-embed-text"
+                )
+                query_embedding = response["embedding"]
+
+                # Query the collection for the 2 most relevant documents
+                results = collection.query(
+                    query_embeddings=[query_embedding],
+                    n_results=2
+                )
+                data.append("\n\n".join(results['documents'][0]))
+            # Extract the relevant excerpts from the results    
+            data = "\n\n".join(data)
+        else:
+            response = ollama.embeddings(
             prompt=user_question,
             model="nomic-embed-text"
-        )
-        query_embedding = response["embedding"]
+            )
+            query_embedding = response["embedding"]
 
-        # Query the collection for the most relevant documents
-        results = collection.query(
-            query_embeddings=[query_embedding],
-            n_results=4
-        )
-        
-        # Extract the relevant excerpts from the results
-        data = "\n\n".join(results['documents'][0])
-        ident = results['ids'][0]
-        print(data)
+            # Query the collection for the most relevant documents
+            results = collection.query(
+                query_embeddings=[query_embedding],
+                n_results=4
+            )
+            # Extract the relevant excerpts from the results
+            data = "\n\n".join(results['documents'][0])
+
+       
+
 
         if user_question:
             # Generate a response using the pre-trained model
-            response = ota_speech_chat_completion(groq_client, model, user_question, data)
-            print(response)
+            response = await ota_speech_chat_completion(groq_client, model, conversation_history, data)
+            stream = []
+            async for chunk in response:
+                block = str(chunk.choices[0].delta.content)
+                if "None" in block:
+                    break
+                else:
+                    print(block, end="")
+                    stream.append(block)
+                    stream_str = "".join(stream)
+            # add chatbot answer to context
+            conversation_history.append(("assistant", stream_str))
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
